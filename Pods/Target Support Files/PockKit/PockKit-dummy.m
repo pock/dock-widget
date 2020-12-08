6ba93fb2314e1850fc8c3776ddf6fbe2003fd464
Pierluigi Galdi <pigi.galdi@gmail.com>
2020-12-08 15:54:56 +0100

    Don't track Pods folders

--- Pods/Target Support Files/PockKit/PockKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PockKit : NSObject
-@end
-@implementation PodsDummy_PockKit
-@end
--- Pods/Target Support Files/Pods-Dock/Pods-Dock-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Dock : NSObject
-@end
-@implementation PodsDummy_Pods_Dock
-@end
